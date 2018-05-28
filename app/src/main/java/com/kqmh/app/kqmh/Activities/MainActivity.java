c102735bfa668d36fa245e6b691574e3bf25efd9
cromwel <cromwellindy@gmail.com>
2018-05-28 15:23:23 +0300

    altered

--- app/src/main/java/com/kqmh/app/kqmh/Activities/MainActivity.java
+++ app/src/main/java/com/kqmh/app/kqmh/Activities/MainActivity.java
@@ -10,42 +10,12 @@ import com.kqmh.app.kqmh.Widgets.FragViewPager;
 
 
 public class MainActivity extends AppCompatActivity {
-
-    private TextView mTextMessage;
-    private BottomBarAdapter adapter;
-    private boolean tabNotification1 = false;
-    private boolean tabNotification2 = false;
-    private boolean tabNotification3 = false;
-    private FragViewPager viewPager;
-    private int currentPosition = 0;
     private ProgressDialog progressDialog;
-
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        viewPager.setPagingEnabled(false);
-
-        progressDialog = new ProgressDialog(this);
-        progressDialog.setCancelable(false);
-
-
-    }
-
-    private void setupViewPager() {
-        adapter = new BottomBarAdapter(getSupportFragmentManager());
-
-        viewPager.setAdapter(adapter);
-        viewPager.setCurrentItem(0);
-        currentPosition = 0;
-    }
-
-    private void closeProgressbar() {
-        if (progressDialog != null) {
-            progressDialog.dismiss();
-        }
-        setupViewPager();
     }
 }
