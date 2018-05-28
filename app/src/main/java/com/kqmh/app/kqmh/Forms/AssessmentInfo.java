0cb2cfc25ccaa9caeba669f8ae6ea4733bec2818
cromwel <cromwellindy@gmail.com>
2018-05-28 15:31:40 +0300

    added new forms resource directory

---
+++ app/src/main/java/com/kqmh/app/kqmh/Forms/AssessmentInfo.java
@@ -0,0 +1,47 @@
+package com.kqmh.app.kqmh.Forms;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.Spinner;
+import android.widget.Toast;
+
+import com.kqmh.app.kqmh.R;
+
+public class AssessmentInfo extends AppCompatActivity {
+
+    private Spinner spinner1;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_assessmentinfo);
+
+        spinner1 = (Spinner) findViewById(R.id.spinner);
+        spinner1.setOnItemSelectedListener(new ItemSelectedListener());
+    }
+
+    public class ItemSelectedListener implements AdapterView.OnItemSelectedListener {
+
+        //get strings of first item
+        String firstItem = String.valueOf(spinner1.getSelectedItem());
+
+        public void onItemSelected(AdapterView<?> parent, View view, int pos, long id) {
+            if (firstItem.equals(String.valueOf(spinner1.getSelectedItem()))) {
+                // ToDo when first item is selected
+            } else {
+                Toast.makeText(parent.getContext(),
+                        "You have selected : " + parent.getItemAtPosition(pos).toString(),
+                        Toast.LENGTH_LONG).show();
+                // Todo when item is selected by the user
+            }
+        }
+
+        @Override
+        public void onNothingSelected(AdapterView<?> arg) {
+
+        }
+
+    }
+}
